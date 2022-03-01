a87240629e90b7334d5a93e9dcbe17b67c388921
James Seconde <jim.seconde@googlemail.com>
2022-03-01 16:49:24 +0000

    Settings now read/write

--- vonage2fa.php
+++ vonage2fa.php
@@ -14,9 +14,6 @@ require __DIR__ . '/vendor/autoload.php';
 $basic = new Client\Credentials\Basic('232130c9', 'mOHPMgmBQBRO8xNB');
 $client = new Client(new Client\Credentials\Container($basic));
 
-add_action('admin_menu', 'test_plugin_setup_menu');
-register_activation_hook(__FILE__, 'db_install');
-
 function db_install()
 {
     global $wpdb;
@@ -35,14 +32,25 @@ function db_install()
     dbDelta($sql);
 }
 
-function test_plugin_setup_menu()
+function db_uninstall()
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'vonage2fa';
+
+    $sql = "DROP TABLE IF EXISTS $table_name";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+    dbDelta( $sql );
+}
+
+function vonage_2fa_setup_menu()
 {
     add_menu_page(
         'Test Plugin Page',
         'Vonage 2FA',
         'manage_options',
-        'test-plugin',
-        'load_admin',
+        'vonage_2fa_plugin',
+        'load_admin_settings',
         'data:image/svg+xml;base64,' . base64_encode('<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
              width="300.000000pt" height="261.000000pt" viewBox="0 0 300.000000 261.000000"
              preserveAspectRatio="xMidYMid meet">
@@ -60,23 +68,57 @@ function test_plugin_setup_menu()
     );
 }
 
-function load_admin()
+function vonage_2fa_register_settings() {
+    register_setting( 'vonage_api_settings_options', 'vonage_api_settings_options', 'vonage_api_settings_options_validate' );
+    add_settings_section( 'api_credentials', 'Vonage API Credentials', 'vonage_plugin_text_helper', 'vonage_2fa_plugin' );
+
+    add_settings_field( 'api_credentials_key', 'API Key', 'api_credentials_key', 'vonage_2fa_plugin', 'api_credentials' );
+    add_settings_field( 'api_credentials_secret', 'API Secret', 'api_credentials_secret', 'vonage_2fa_plugin', 'api_credentials' );
+    add_settings_field( 'api_credentials_phone_number', 'Phone Number', 'api_credentials_phone_number', 'vonage_2fa_plugin', 'api_credentials' );
+}
+
+function vonage_api_settings_options_validate()
+{
+    return true;
+}
+
+function vonage_plugin_text_helper() {
+    echo '<p>You will need a valid API Key/Secret credentials pair from your Vonage Dashboard, and a working phone number.</p>';
+}
+
+function api_credentials_key() {
+    $options = get_option( 'vonage_api_settings_options' );
+    echo "<input id='api_credentials_key' name='vonage_api_settings_options[api_credentials_key]' type='text' value='" . esc_attr( $options['api_credentials_key'] ) . "' />";
+}
+
+function api_credentials_secret() {
+    $options = get_option( 'vonage_api_settings_options' );
+    echo "<input id='api_credentials_secret' name='vonage_api_settings_options[api_credentials_secret]' type='text' value='" . esc_attr( $options['api_credentials_secret'] ) . "' />";
+}
+
+function api_credentials_phone_number() {
+    $options = get_option( 'vonage_api_settings_options' );
+    echo "<input id='api_credentials_phone_number' name='vonage_api_settings_options[api_credentials_phone_number]' type='text' value='" . esc_attr( $options['api_credentials_phone_number'] ) . "' />";
+}
+
+function load_admin_settings()
 {
     echo "
     <img src='" . plugin_dir_url(__FILE__) . "assets/logo-large.png' alt='Vonage logo'>
     <h1>Built in 2FA</h1>
     <p>A text is sent out with a 2FA code to the user for each Admin panel login attempt.</p>
     <div>
-        <h2>Vonage API Keys</h2>
-        <form>
-            <label for='api_key'>API Key</label>
-            <input type='text' name='api_key'/>
-            <label for='api_key'>API Secret</label>
-            <input type='text' name='api_secret'/>
-            <label for='active'>2FA Active</label>
-            <input type='checkbox' name='active'/>
-            <input type='submit'>
+        <form action='options.php' method='post'>";
+            settings_fields('vonage_api_settings_options');
+            do_settings_sections('vonage_2fa_plugin');
+            submit_button();
+    echo "
         </form>
     </div>
     ";
 }
+
+add_action('admin_menu', 'vonage_2fa_setup_menu');
+register_activation_hook(__FILE__, 'db_install');
+register_deactivation_hook(__FILE__, 'db_uninstall');
+add_action('admin_init', 'vonage_2fa_register_settings');
